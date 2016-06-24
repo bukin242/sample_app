52a48647737e0d7fd49e55591f2e6cc88b970517
Aleksey Bukin <bukin242@yandex.ru>
2016-06-24 11:03:34 +0300

    Add simple tag

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -47,5 +47,21 @@ describe "User pages" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
     end
+
+    describe "after saving the user" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+
+        click_button submit
+      end
+
+      let(:user) { User.find_by(email: 'user@example.com') }
+
+      it { should have_title(user.name) }
+      it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+    end
   end
 end
